5b2d905fc8b82cb480802680f8e4a3dc022cf0ac
Root6030 <50948896+Root6030@users.noreply.github.com>
2020-04-15 15:37:23 +0100

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+<!--Script for animated slider-->
+            
+                var myIndex = 0;
+                carousel();
+
+                function carousel() {
+                    var i;
+                    var x = document.getElementsByClassName("slider-image");
+                        for (i = 0; i < x.length; i++) {
+                            x[i].style.display = "none";
+                        }            
+                myIndex++;
+                    if (myIndex > x.length) {myIndex = 1}    
+                        x[myIndex-1].style.display = "block";  
+                            setTimeout(carousel, 5000);
+                }   
+            
